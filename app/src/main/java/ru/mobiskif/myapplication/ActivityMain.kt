2af1108e0fbf9edbdb5e7b76fa45b6c6682bdde2
john <mobiskif@hotmail.com>
2018-11-11 21:31:03 +0300

    5/11/18

--- app/src/main/java/ru/mobiskif/myapplication/ActivityMain.kt
+++ app/src/main/java/ru/mobiskif/myapplication/ActivityMain.kt
@@ -3,17 +3,12 @@ package ru.mobiskif.myapplication
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.RadioGroup
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity1.*
-import kotlinx.android.synthetic.main.activity1draw.*
 import kotlinx.android.synthetic.main.app_bar.*
-import kotlinx.android.synthetic.main.content.*
+import kotlinx.android.synthetic.main.main_activiry.*
 
 class ActivityMain : AppCompatActivity() {
 
@@ -21,33 +16,34 @@ class ActivityMain : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity1draw)
+        setContentView(R.layout.main_activiry)
         //setSupportActionBar(detail_toolbar)
+        //NavHostFragment.findNavController(mynav).navigate(R.id.action_blankFragment0_to_blankFragment1)
+        /*
 
-        //mDrawerLayout = drawer_layout
-
-        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).show() }
+        nav_view.setNavigationItemSelectedListener { menuItem ->
+            onOptionsItemSelected(menuItem)
+            drawer_layout.closeDrawers()
+            true
+        }
 
         val toggle = ActionBarDrawerToggle(this, drawer_layout, detail_toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
-        nav_view.setNavigationItemSelectedListener { menuItem ->
-            menuItem.isChecked = true
-            drawer_layout.closeDrawers()
-            true
-        }
-
-
-
         radiogoup.setOnCheckedChangeListener { radioGroup: RadioGroup, i: Int ->
             Snackbar.make(radioGroup, "" + i, Snackbar.LENGTH_SHORT).show()
-            NavHostFragment.findNavController(nav_host_fragment).navigate(R.id.action_blankFragment0_to_blankFragment1)
+            //NavHostFragment.findNavController(nav_host_fragment).navigate(R.id.action_blankFragment0_to_blankFragment1)
         }
+*/
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, detail_toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
 
+        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).show() }
     }
 
-    override fun onSupportNavigateUp() = Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
+    //override fun onSupportNavigateUp() = Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.draw_menu, menu)
@@ -57,16 +53,17 @@ class ActivityMain : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_slideshow -> {
-                NavHostFragment.findNavController(nav_host_fragment).navigate(R.id.action_blankFragment0_to_blankFragment1)
+                //NavHostFragment.findNavController(nav_host_fragment).navigate(R.id.action_blankFragment0_to_blankFragment1)
                 return true
             }
             R.id.nav_manage -> {
-                NavHostFragment.findNavController(nav_host_fragment).navigateUp()
+                //NavHostFragment.findNavController(nav_host_fragment).navigateUp()
                 return true
             }
             else -> return super.onOptionsItemSelected(item)
         }
     }
+
 }
 
 /*
@@ -75,4 +72,12 @@ class ActivityMain : AppCompatActivity() {
                 .replace(R.id.nav_host_fragment, BlankFragment0.newInstance("qwe", "asd"))
                 .commitNow()
         }
+*/
+
+/*
+nav_view.setNavigationItemSelectedListener { menuItem ->
+    menuItem.isChecked = true
+    drawer_layout.closeDrawers()
+    true
+}
 */
--- app/src/main/java/ru/mobiskif/myapplication/BlankFragment0.kt
+++ app/src/main/java/ru/mobiskif/myapplication/BlankFragment0.kt
@@ -40,6 +40,7 @@ class BlankFragment0 : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_blank_fragment0, container, false)
     }
 
