d4b658ba05ff4284fdac9f2b5f529a53713abca9
john <mobiskif@hotmail.com>
2019-01-12 22:14:28 +0300

    12/01/19

--- app/src/main/java/ru/mobiskif/RecyclerAdapter_Talon.kt
+++ app/src/main/java/ru/mobiskif/RecyclerAdapter_Talon.kt
@@ -1,7 +1,9 @@
 package ru.mobiskif
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.RecyclerView
@@ -23,6 +25,25 @@ class RecylcerAdapter_Talon(val items: List<Map<String, Any>>, private val fragm
         holder.itemView.text1.text = items[position].get("День недели").toString()
         holder.itemView.text2.text = items[position].get("Дата").toString()
         holder.itemView.text3.text = items[position].get("Время работы").toString()
+
+        var mm = items[position].get("Талоны") as Map<String, String>
+        var lay = holder.itemView.lay01
+
+        holder.itemView.text01.text="=="
+        mm.forEach {
+            //Log.d("jop","${it.key} = ${it.value}")
+            //var s = holder.itemView.text01.text.toString()
+            //s = s + it.value + "\n\n"
+            //holder.itemView.text01.text = s
+
+            var tv = TextView(fragm.context)
+            tv.layoutParams = holder.itemView.text01.layoutParams
+
+            tv.setText("${it.key} = ${it.value}")
+            lay.addView(tv)
+
+        }
+
         //holder.itemView.text2.text = "$position"
 
         holder.itemView.setOnClickListener {
