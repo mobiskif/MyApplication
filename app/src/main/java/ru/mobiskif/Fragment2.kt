d24a05a6e8119e67962f5c5690df1e2e779de19a
john <mobiskif@hotmail.com>
2019-01-12 16:08:16 +0300

    12/01/19

--- app/src/main/java/ru/mobiskif/Fragment2.kt
+++ app/src/main/java/ru/mobiskif/Fragment2.kt
@@ -1,6 +1,8 @@
 package ru.mobiskif
 
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +10,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
+import com.prolificinteractive.materialcalendarview.CalendarDay
 import kotlinx.android.synthetic.main.fragment_2.*
+import java.util.*
 
 
 class Fragment2 : Fragment() {
@@ -30,13 +34,16 @@ class Fragment2 : Fragment() {
         recyclerTalon.adapter = RecylcerAdapter(mModel.getTalonList().value!!, this, R.layout.card_talon, mModel)
         activity!!.title = mModel.cspecname.value + ' ' + mModel.cdoctorname.value
 
+        calendarView.selectedDate=CalendarDay.today()
+        //calendarView.setDateSelected()
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: ru.mobiskif.databinding.Fragment2Binding = DataBindingUtil.inflate(inflater, R.layout.fragment_2, container, false)
         binding.model1 = mModel
-        //return inflater.inflate(R.layout.fragment_2, container, false)
         return binding.root
+        //return inflater.inflate(R.layout.fragment_2, container, false)
     }
 
 }
