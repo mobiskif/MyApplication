466e286231a9b73abdf72532d5d17cf66b1158b4
john <mobiskif@hotmail.com>
2019-02-03 13:43:35 +0300

    03/02/19

--- app/src/main/java/ru/mobiskif/Fragment3.kt
+++ app/src/main/java/ru/mobiskif/Fragment3.kt
@@ -7,8 +7,10 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.fragment_3.*
 
-class Fragment3 : Fragment() {
+class Fragment3 : Fragment(), View.OnClickListener {
 
     lateinit var model: MyViewModel
     private lateinit var binding: ru.mobiskif.databinding.Fragment3Binding
@@ -21,8 +23,15 @@ class Fragment3 : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_3, container, false)
         binding.model3 = model
+        binding.buttonYes.setOnClickListener(this)
         return binding.root
         //return inflater.inflate(R.layout.fragment_1, container, false);
     }
 
+    override fun onClick(v: View?) {
+        if (v!!.id==R.id.buttonYes) {
+            NavHostFragment.findNavController(this).navigate(R.id.Fragment1)
+        }
+
+    }
 }
